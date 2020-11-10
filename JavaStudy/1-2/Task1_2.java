75f04a030c8f746b6753ec756eea60f066c3e70e
andk73441 <72036730+andk73441@users.noreply.github.com>
2020-11-10 17:00:46 +0900

    配列　課題の提出
    Task1_2.javaの作成
    Task1_2.classの作成

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,20 @@
+public class Task1_2 {
+    public static void main(String[] args) {
+
+        String[] country = {"JAPAN","AMERICA","KOREA"};
+
+        System.out.println(country.length);
+
+        String[] strArray = {"りんご","もも","ぶどう"};
+
+        System.out.println(strArray[1]);
+
+        //10,20,30,40,50 を要素の値（初期値）とする
+        //要素数５の intArray という名前のint型配列を宣言。
+        int[] intArray = {10,20,30,40,50};
+        //添え字（インデックス）が１の要素の値と、４の要素の値を表示
+        System.out.println(intArray[1] + intArray[4]);
+
+    }
+    
+}
